720c7ea614024a6e0375547a40a03c02f0184809
leonnelc <leonelcortes2424@gmail.com>
2024-06-19 23:57:15 -0300

    Add "show_number" config option and functionality

--- lua/battery/battery.lua
+++ lua/battery/battery.lua
@@ -5,37 +5,40 @@ M.color = ""
 M.icon = ""
 
 function M.get_battery_table()
-	return M.table
+  return M.table
 end
 
 function M.get_battery_status()
-	return M.status
+  return M.status
 end
 
 function M.get_battery_icon()
-	return M.icon
+  return M.icon
 end
 
 function M.get_battery_color()
-	return M.color
+  return M.color
 end
 
 function M.update_status()
-	local config = require("battery.config").current
-	if M.table.percentage == nil then
-		return
-	end
-	M.icon = require("battery.misc").get_icon(M.table.percentage)
-	M.color = require("battery.misc").get_color(M.table.percentage)
-	M.status = "" .. M.table.percentage
+  local config = require("battery.config").current
+  if M.table.percentage == nil then
+    return
+  end
+  M.icon = require("battery.misc").get_icon(M.table.percentage)
+  M.color = require("battery.misc").get_color(M.table.percentage)
+  M.status = ""
 
-	if config.show_percent == true then
-		M.status = M.status .. "%%"
-	end
+  if config.show_number then
+    M.status = M.status .. M.table.percentage
+  end
+  if config.show_percent == true then
+    M.status = M.status .. "%%"
+  end
 
-	if config.show_icon == true then
-		M.status = M.icon .. M.status
-	end
+  if config.show_icon == true then
+    M.status = M.icon .. M.status
+  end
 end
 
 return M
--- lua/battery/config.lua
+++ lua/battery/config.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 local default_config = {
-	update_rate_seconds = 30,
-	upower_path = "/org/freedesktop/UPower/devices/battery_BAT1",
-	show_percent = true,
-	show_icon = false,
+  update_rate_seconds = 30,
+  upower_path = "/org/freedesktop/UPower/devices/battery_BAT1",
+  show_number = true,
+  show_percent = true,
+  show_icon = false,
 }
 M.current = default_config
 M.from_user_opts = function(user_opts)
-	M.current = user_opts and vim.tbl_deep_extend("force", default_config, user_opts) or default_config
+  M.current = user_opts and vim.tbl_deep_extend("force", default_config, user_opts) or default_config
 end
 
 return M
